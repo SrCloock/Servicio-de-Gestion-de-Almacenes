ad2fc2beb8c038ec28e62539cb9957ac3df158bb
Alex Rivas <alexrb.dev@gmail.com>
2025-06-09 08:15:26 +0200

    Solo backend

--- Nueva carpeta/frontend/nuevoPedido.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const params = new URLSearchParams(window.location.search);
-  const nombre = params.get('nombre');
-  document.getElementById('nombreCliente').value = nombre || '';
-});
